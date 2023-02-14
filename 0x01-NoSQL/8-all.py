e45e57b9fc699493d755fb0c8bed799fcb6edfc2
tobexint <tobexint@gmail.com>
2023-02-14 14:52:59 -0800

    mongodb

---
+++ 0x01-NoSQL/8-all.py
@@ -0,0 +1,14 @@
+#!/usr/bin/env python3
+"""
+This module contains a python function that list all document in a collection
+"""
+
+
+def list_all(mongo_collection):
+    """
+    this method lists all documents in a collection
+    Args: mongo_collection as the pymongo collection object
+    Returns:
+         list of documents or empty list if no document is in the collection
+    """
+    return mongo_collection.find()
---
+++ 0x01-NoSQL/README.md
@@ -0,0 +1,1 @@
+NoSQL database unlike RDBMS stores data as collections or objects.
